4a993173338ef7d9abfa13f25d12129033662a3d
Sarah Harrs <sarahharrs@gmail.com>
2019-08-03 20:37:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+
+    def initialize(knowledge)
+      @knowledge= []
+    end
+
+    def learn("string")
+      "string" >> @knowledge
+    end
+
+    def knowledge
+      @knowledge.join(",")
+    end
 end
